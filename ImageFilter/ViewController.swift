33c611b897ef0a6ba686687c868dcd2be93e0591
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-05-09 20:24:10 +0700

    initial commit

--- ImageFilter/ViewController.swift
+++ ImageFilter/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  ImageFilter
@@ -8,13 +9,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var importImage: UIButton!
+    @IBOutlet weak var takePicture: UIButton!
+    
+//    var fileUrl: URL?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func takePictureAction(_ sender: UIButton) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.camera
+        image.allowsEditing = false
+        
+        self.present(image, animated: true){
+            
+        }
+    }
+    
+    @IBAction func importImageAction(_ sender: UIButton) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = false
+        
+        self.present(image, animated: true){
+            
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//            photo.image = image
+            filterSepia(image: image)
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func filterSepia(image: UIImage) {
+        let beginImage = CIImage(image: image)
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(beginImage, forKey: kCIInputImageKey)
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        
+        let newImage = UIImage(ciImage: (filter?.outputImage)!)
+        self.photo.image = newImage
+    }
 
 }
 
